7390c9896a978460cbf508e4f597066eb10a3237
Samuel <samueliagocabral@gmail.com>
2026-06-15 17:05:57 -0300

    arquivos backend

---
+++ backend/core/config/api_routers.py
@@ -0,0 +1,21 @@
+from categories.api.views import CategoryViewSet
+from django.urls import include, path
+from expenses.api.views import ExpenseViewSet
+from periods.api.views import PeriodExportViewSet, PeriodViewSet
+from rest_framework import routers
+from users.api.views import LogoutView
+
+router = routers.SimpleRouter()
+router.register('categories', CategoryViewSet, 'category')
+router.register('periods', PeriodViewSet, 'period')
+router.register('expenses', ExpenseViewSet, 'expense')
+
+
+urlpatterns = [
+    path('auth/', include('djoser.urls.jwt')),
+    path('auth/', include('djoser.urls')),
+    path('auth/logout/', LogoutView.as_view(), name='logout'),
+    path('periods/export/', PeriodExportViewSet.as_view(), name='period-export'),
+]
+
+urlpatterns += router.urls
